a490782f598d9c3df224c9f7bba4a3bfbc6e9045
TraumDE <eugene301workmaik@gmail.com>
2026-07-13 04:15:10 +0300

    feat(parser): add base ast parser

--- cl++/src/index.ts
+++ cl++/src/index.ts
@@ -1,4 +1,6 @@
 import fs from "fs";
+import path from "path";
+import peggy from "peggy";
 
 const main = () => {
   const filePath = process.argv[2];
@@ -15,6 +17,15 @@ const main = () => {
 
   try {
     const sourceCode = fs.readFileSync(filePath, "utf-8");
+
+    const grammarPath = path.resolve("src/grammar.peggy");
+    const grammar = fs.readFileSync(grammarPath, "utf-8");
+
+    const parser = peggy.generate(grammar);
+
+    const ast: string = parser.parse(sourceCode);
+
+    console.log(JSON.stringify(ast, null, 2));
 
     console.log(sourceCode);
   } catch (error: unknown) {
