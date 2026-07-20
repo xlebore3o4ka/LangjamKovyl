cc8ac9b38a09c0b4237470239f982df755cbcff6
TraumDE <eugene301workmaik@gmail.com>
2026-07-20 03:01:30 +0300

    feat: improve clear screen module

--- cl++/projects/chat/clear_screen.erl
+++ cl++/projects/chat/clear_screen.erl
@@ -4,7 +4,7 @@
 clear_screen(Client) ->
     try
         clx_std:print("User console cleared!"),
-    gen_tcp:send(Client, "\e[2J\e[H")
+    gen_tcp:send(Client, "\e[r\e[2J\e[H")
     catch
         throw:{'__clx_return', ReturnValue} -> 
         ReturnValue
